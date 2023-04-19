1867b846c5b8c3b766ad87e5245b0772fb9cf691
SeijDeLeon <sdeleon@berkeley.edu>
2023-04-19 13:44:53 -0700

    refactor tasks to form elements with edit feature

--- src/Task.js
+++ src/Task.js
@@ -1,15 +1,58 @@
+import { useState } from 'react';
+
 export default function Task({task}) {
   //this component is for a single task element
 
-  //user can sel
+  //clicking the pencil icon will:
+  //1: change styling behind text to white
+  //2: allow user to modify text with white background
+  //3:display a 'save changes' button that will submit task data to PUT
 
+  const defaultTask = {
+    title: task.title,
+    description: task.description,
+    dueDate: task.dueDate,
+    completionStatus: task.completionStatus
+  };
+  const [data, setData] = useState(defaultTask)
+
+  const [bgColorClass, setBgColorClass] = useState('transparent');
+
+  const [readOnly, setReadOnly] = useState(true);
+  const [showButton, setShowButton] = useState('visible')
+
+  const changeHandler = e => {
+    setData( prevData => {
+      return {...prevData, [e.target.name]: e.target.value}
+    })
+  }
+
+  const saveChangesHandler = () => {
+    //submit a PUT request with current form values
+  }
+
+  const cancelChangesHandler = () => {
+    //reset form elements to defaults
+    setData(defaultTask);
+    setReadOnly(true);
+    setBgColorClass('transparent');
+  }
+
+  const editClickHandler = () => {
+    setReadOnly(!readOnly);
+    setBgColorClass(bgColorClass === 'transparent' ? 'white' : 'transparent');
+  }
   return (
     <div className='task'>
-      <img src="pencil.png" alt="edit"></img>
-      <h3 className='text-center'>{task.title}</h3>
-      <p>{task.description}</p>
-      <p>{`Due Date: ${task.dueDate}`}</p>
-      <p>{task.completionStatus}</p>
+      <img src="pencil.png" onClick={editClickHandler} alt="edit"></img>
+      {!readOnly ? <button onClick={saveChangesHandler}>Save Changes</button> : null}
+      {!readOnly ? <button onClick={cancelChangesHandler}>Cancel Changes</button> : null}
+      <form onChange={changeHandler}>
+        <input className={bgColorClass} readOnly={readOnly} name='title' value={data.title}></input>
+        <input className={bgColorClass} readOnly={readOnly} name='description' value={data.description}></input>
+        <input className={bgColorClass} readOnly={readOnly} name='dueDate' value={data.dueDate}></input>
+        <input className={bgColorClass} readOnly={readOnly} name='completionStatus' value={data.completionStatus}></input>
+      </form>
     </div>
   )
 }
